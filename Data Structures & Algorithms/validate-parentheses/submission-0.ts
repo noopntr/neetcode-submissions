0e5a17fed4b050518c578d995b817cb9aa245d53
Nodo Kobakhidze <kobakhidz3@gmail.com>
2026-06-17 15:19:32 +0400

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @return {boolean}
+   */
+  isValid(s: string): boolean {
+    while (s.includes("()") || s.includes("{}") || s.includes("[]")) {
+      s = s.replace("()", "");
+      s = s.replace("{}", "");
+      s = s.replace("[]", "");
+    }
+
+    return s === "";
+  }
+}
