724627a5bc2770539fb1d80c6f22f775a685ef33
Nodo Kobakhidze <kobakhidz3@gmail.com>
2026-06-27 15:03:27 +0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.ts
@@ -0,0 +1,31 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} k
+   * @return {number[]}
+   */
+  topKFrequent(nums: number[], k: number): number[] {
+    const count = {};
+    const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+    for (const n of nums) {
+      count[n] = (count[n] || 0) + 1;
+    }
+
+    for (const n in count) {
+      freq[count[n]].push(parseInt(n));
+    }
+
+    const res: number[] = [];
+
+    for (let i = freq.length - 1; i > 0; i--) {
+      for (const n of freq[i]) {
+        console.log(n, freq[i]);
+        res.push(n);
+        if (res.length === k) {
+          return res;
+        }
+      }
+    }
+  }
+}
