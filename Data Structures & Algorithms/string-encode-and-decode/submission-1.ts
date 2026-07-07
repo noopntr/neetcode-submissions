177f3222cd8d05cb6ad0e48e2205cc2f78ac5e42
Nodo Kobakhidze <kobakhidz3@gmail.com>
2026-07-07 16:15:48 +0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.ts
@@ -0,0 +1,37 @@
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs: string[]): string {
+    const res = [];
+    for (let s of strs) {
+      res.push(String(s.length), "#", s);
+    }
+
+    return res.join("");
+  }
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str: string): string[] {
+    const res = [];
+    let i = 0;
+
+    while (i < str.length) {
+      let j = i;
+      while (str[j] !== "#") {
+        j++;
+      }
+      let length = parseInt(str.substring(i, j));
+      i = j + 1;
+      j = i + length;
+      res.push(str.substring(i, j));
+      i = j;
+    }
+
+    return res;
+  }
+}
