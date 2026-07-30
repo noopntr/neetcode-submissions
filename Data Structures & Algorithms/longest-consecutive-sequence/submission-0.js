f59ccaed6383a41dd04c3deb11fcfa6fc2a85765
Nodo Kobakhidze <kobakhidz3@gmail.com>
2026-07-30 07:32:53 +0400

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  longestConsecutive(nums) {
+    let res = 0;
+    const store = new Set(nums);
+
+    for (let num of nums) {
+      let streak = 0,
+        curr = num;
+
+      console.log(streak, curr, num);
+
+      while (store.has(curr)) {
+        streak++;
+        curr++;
+      }
+      res = Math.max(res, streak);
+    }
+    return res;
+  }
+}
