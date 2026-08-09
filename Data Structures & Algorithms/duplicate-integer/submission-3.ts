70a31e3447a886b96dbe52032a26e8e49e07ff4b
Nodo Kobakhidze <kobakhidz3@gmail.com>
2026-08-10 03:54:56 +0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        const numsSet = new Set(nums)
+
+        if(nums.length !== numsSet.size) {
+            return true
+        }
+
+        return false
+    }
+}
